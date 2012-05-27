3534b40de861ae3aca4f7f53d1f90a144a2b0baf
Christof Geisler <github@christof-geisler.de>
2012-05-27 22:33:19 +0200

    Add Doxygen html and latex

---
+++ Doxygen/project.h
@@ -0,0 +1,31 @@
+/** @mainpage Documentation
+
+    Copyright (C) 2012 by
+    Fabian Deitelhoff <FH@FabianDeitelhoff.de> and
+    Christof Geisler <christof.geisler@stud.fh-swf.de>
+
+    %SolarSystemSimulation is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    %SolarSystemSimulation is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with %SolarSystemSimulation.  If not, see <http://www.gnu.org/licenses/>.
+
+    @author Fabian Deitelhoff <FH@FabianDeitelhoff.de>
+    @author Christof Geisler <christof.geisler@stud.fh-swf.de>
+
+
+    Circumference of an ellipse according to Ramanujan:
+
+   \f[ u \approx (a + b) \cdot \pi \cdot ( 1 + \frac{ 3 \lambda^2}{10 + \sqrt{4 - 3 \lambda^2}}) \quad \hspace{0.5cm} with \hspace{0.5cm} \quad \lambda = \frac{a-b}{a+b} \f]
+
+    @image html about_small.jpg "Planets of our solar system"
+
+    @image latex about.jpg "Planets of our solar system" width=12cm
+*/
